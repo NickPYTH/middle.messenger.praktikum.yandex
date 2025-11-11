b1aedfd43cbc77a4bb49470b5ddbc243f769ccda
Николай С <nicksrnk@gmail.com>
2025-11-11 20:52:14 +0300

    doing smth

---
+++ src/components/button/button.ts
@@ -0,0 +1,47 @@
+import Block, {BlockEvent, IBlockEvents} from '../../reactor/Block';
+import {validateField} from '../../utils/validation';
+
+type ButtonType = {
+    id?: string;
+    name?: string;
+    type?: string;
+    [key: string]: unknown;
+}
+
+export default class Button extends Block<ButtonType> {
+    constructor(props: ButtonType) {
+        const blurHandler: BlockEvent = (e: Event) => {
+            this.handleBlur(e as FocusEvent);
+        };
+
+        const events: IBlockEvents = {
+            ...(props.events || {}),
+            blur: blurHandler,
+            onClick: () => {alert('kek')}
+        };
+        const inputClass = `input ${props.className || ''}${props.error ? ' errorOld' : ''}`;
+
+        super({
+            ...props,
+            inputClass,
+            events
+        });
+    }
+
+    handleBlur(e: Event): void {
+        const input = e.target as HTMLInputElement;
+        const { name, value } = input;
+        const error = validateField(name, value);
+        this.setProps({
+            error,
+            value,
+            inputClass: `input ${this.props.className || ''}${error ? ' errorOld' : ''}`
+        });
+    }
+
+    render(): string {
+        return `
+            <button ${this.props.type || 'type="submit"'} class=${this.props.className}>${this.props.name}</button>
+        `;
+    }
+}
